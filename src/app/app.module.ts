f3155feb0a4ff7cbf2f36b5d2302c45707fad3d7
francis rochon <francis.rochon99@gmail.com>
2017-06-29 22:20:12 -0400

    fix error to deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AUTH_PROVIDERS } from 'angular2-jwt';
 
 import { AppComponent } from './app.component';
 import { ToolBar } from './shared/component/toolbar/toolbar.component';
@@ -56,7 +55,6 @@ import { CallbackComponent } from './callback/callback.component';
     PlayersInfoService,
     DraftPlayerService,
     AuthService,
-    AUTH_PROVIDERS,
     AuthGuard,
   ],
   bootstrap: [AppComponent]
