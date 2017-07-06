e6918214517173482eab99cb3c7dae7a163b3a39
francis rochon <francis.rochon99@gmail.com>
2017-07-06 13:33:48 -0400

    correction in paths to access api

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,29 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 
+import { environment } from '../../../environments/environment';
 import { User } from '../models/user';
 
 @Injectable()
 export class UserService {
     constructor(private http: Http) { }
 
+    //TODO change to add domain path and version for const in http trequests
     getAll() {
-        return this.http.get('v1/users').map((response: Response) => response.json());
+        return this.http.get(environment.apiUrl + 'api/nhl/poolApp/v1/users').map((response: Response) => response.json());
     }
 
     getById(_id: string) {
-        return this.http.get('v1/users/' + _id).map((response: Response) => response.json());
+        return this.http.get(environment.apiUrl + 'api/nhl/poolApp/v1/users/' + _id).map((response: Response) => response.json());
     }
 
     create(user: User) {
-        return this.http.post('v1/users/register', user);
+        return this.http.post(environment.apiUrl + 'api/nhl/poolApp/v1/users/register', user);
     }
 
     update(user: User) {
-        return this.http.put('v1/users/' + user._id, user);
+        return this.http.put(environment.apiUrl + 'api/nhl/poolApp/v1/users/' + user._id, user);
     }
 
     delete(_id: string) {
-        return this.http.delete('v1/users/' + _id);
+        return this.http.delete(environment.apiUrl + 'api/nhl/poolApp/v1/users/' + _id);
     }
 }
