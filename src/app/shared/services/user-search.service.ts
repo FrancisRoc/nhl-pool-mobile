f10348d0b4c127bbb9f95e445b01890481db5900
francis rochon <francis.rochon99@gmail.com>
2017-07-14 09:37:42 -0400

    fix empty members when we have not selected a pool + fix not initialized current stats in players list dropdown

--- src/app/shared/services/user-search.service.ts
+++ src/app/shared/services/user-search.service.ts
@@ -14,7 +14,7 @@ export class UserSearchService {
   private currentUser: User;
   private opponents: Opponent[];
 
-  constructor(private http: Http, private poolService: PoolService) { 
+  constructor(private http: Http, private poolService: PoolService) {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
   }
 
@@ -23,7 +23,12 @@ export class UserSearchService {
     console.log("API call: " + environment.apiUrl + `api/nhl/poolApp/v1/users?name=${term}`);
 
     // Update opponents
-    this.opponents = this.poolService.getCurrentPool().members;
+    if (this.poolService.getCurrentPool()) {
+      this.opponents = this.poolService.getCurrentPool().members;
+    } else {
+      this.opponents = [];
+    }
+
     return this.http
       .get(environment.apiUrl + `api/nhl/poolApp/v1/users?name=${term}`)
       .map(response => {
@@ -32,7 +37,7 @@ export class UserSearchService {
         for (let i = 0; i < this.opponents.length; i++) {
           excludeCurrentUser = excludeCurrentUser.filter(item => item.username !== this.opponents[i].username);
         }
-        return excludeCurrentUser as User[] 
+        return excludeCurrentUser as User[]
       });
   }
 
